08c4236b10fee3e0f284e2e52f6da9df2d95b30b
user-Gyrus <72992661+user-Gyrus@users.noreply.github.com>
2024-06-18 13:04:32 +0530

    Add files via upload

---
+++ Sorting Techniques/bubbleSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "After Using bubble sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using Bubble Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubble_sort(arr, n);
+    return 0;
+}
+
+//time complexities - 
+// best case - O(N), worst and avg cases - O(N^2)
+
+//space complexities - 
+//O(1)
